8c71b8326683d5282ea3b8d314f03b976ce21732
tutao <542366504@qq.com>
2022-03-27 00:35:54 +0800

    html基础

---
+++ html/jdserver.py
@@ -0,0 +1,14 @@
+# -*- coding:utf-8 -*-
+import socket
+
+sock = socket.socket()
+sock.bind(("127.0.0.1", 8888))
+sock.listen(5)
+while True:
+    conn, addr = sock.accept()
+    data = conn.recv(1024)
+    print('has data:', data.decode())
+    with open('index.html') as f:
+        html = f.read()
+    conn.send(("HTTP/1.1 200 ok \r\n\r\n"+html).encode())
+    conn.close()
